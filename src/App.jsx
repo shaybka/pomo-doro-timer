f60b833f48bffb2b9dbc5e34c800579c61dc0b74
aden mohamoud <adenmohmuud@gmail.com>
2024-09-14 21:43:41 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Body from './components/body/body'
+import Body from './components/body/cont'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
